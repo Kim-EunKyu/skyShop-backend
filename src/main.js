80d268be4f8ad73f335389b9842bf6b29662f7f5
Kim-EunKyu <rladmsrb96@naver.com>
2021-02-06 13:00:27 +0900

    cors 허용

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import api from "./api";
 import jwtMiddleware from "./lib/jwtMiddleware";
@@ -32,6 +33,12 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(jwtMiddleware);
+app.use(
+  cors({
+    origin: "https://compassionate-austin-862a3c.netlify.app/", // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+  })
+);
 
 //router 설정
 app.use("/api", api);
